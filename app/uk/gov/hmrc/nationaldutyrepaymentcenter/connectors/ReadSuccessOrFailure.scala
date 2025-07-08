c6190c51ada63a96edf6d3ae97cd75ef784a45e8
Ming Sun <86245839+sunmingdh@users.noreply.github.com>
2025-07-08 14:16:58 +0100

    DDCYLS-2658: refactor ReadSuccessOrFailure (#90)

--- app/uk/gov/hmrc/nationaldutyrepaymentcenter/connectors/ReadSuccessOrFailure.scala
+++ app/uk/gov/hmrc/nationaldutyrepaymentcenter/connectors/ReadSuccessOrFailure.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 HM Revenue & Customs
+ * Copyright 2025 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,60 +20,48 @@ import play.api.libs.json.{JsError, JsSuccess, Reads}
 import play.mvc.Http.{HeaderNames, MimeTypes}
 import uk.gov.hmrc.http.HttpReads.Implicits._
 import uk.gov.hmrc.http.{HttpReads, HttpResponse, JsValidationException, TooManyRequestException, UpstreamErrorResponse}
-
 import scala.util.Try
 
 abstract class ReadSuccessOrFailure[A, S <: A: Reads, F <: A: Reads](fallback: (Int, String) => A)(implicit
   mf: Manifest[A]
 ) {
 
-  implicit val readFromJsonSuccessOrFailure: HttpReads[A] =
-    HttpReads[HttpResponse].flatMap {
-      response =>
-        response.status match {
-          case 429 =>
-            throw new TooManyRequestException(response.header("Retry-After").getOrElse(""))
-          case status =>
-            if (response.body.isEmpty)
-              HttpReads.pure(fallback(status, "Error: empty response"))
-            else
-              response.header(HeaderNames.CONTENT_TYPE) match {
-                case None =>
-                  HttpReads.pure(fallback(status, "Error: missing content-type header"))
-
-                case Some(MimeTypes.JSON) =>
-                  if (status >= 200 && status < 300)
-                    Try[HttpReads[A]](implicitly[Reads[S]].reads(response.json) match {
-                      case JsSuccess(value, _) => HttpReads.pure(value)
-                      case JsError(errors) =>
-                        HttpReads.ask.map {
-                          case (method, url, _) =>
-                            throw new JsValidationException(method, url, mf.runtimeClass, errors.toString)
-                        }
-                    })
-                      .fold(e => HttpReads.pure(fallback(status, e.getMessage)), identity)
-                  else if (status >= 400)
-                    Try[HttpReads[A]](implicitly[Reads[F]].reads(response.json) match {
-                      case JsSuccess(value, _) => HttpReads.pure(value)
-                      case JsError(errors) =>
-                        HttpReads.ask.map {
-                          case (method, url, _) =>
-                            throw new JsValidationException(method, url, mf.runtimeClass, errors.toString)
-                        }
-                    })
-                      .fold(e => HttpReads.pure(fallback(status, e.getMessage)), identity)
-                  else
-                    throw UpstreamErrorResponse(s"Unexpected response status $status", 500)
-
-                case other =>
-                  throw UpstreamErrorResponse(
-                    s"Unexpected response type of status $status, expected application/json but got ${other
-                        .getOrElse("none")} with body:\n${response.body}",
-                    500
-                  )
-              }
+  private def parseJson[T <: A: Reads](response: HttpResponse, status: Int): HttpReads[A] =
+    Try[HttpReads[A]](implicitly[Reads[T]].reads(response.json) match {
+      case JsSuccess(value, _) => HttpReads.pure(value)
+      case JsError(errors) =>
+        HttpReads.ask.map {
+          case (method, url, _) =>
+            throw new JsValidationException(method, url, mf.runtimeClass, errors.toString)
         }
+    }).fold(e => HttpReads.pure(fallback(status, e.getMessage)), identity)
 
+  implicit val readFromJsonSuccessOrFailure: HttpReads[A] = HttpReads[HttpResponse].flatMap { response =>
+    response.status match {
+      case 429 =>
+        throw new TooManyRequestException(response.header("Retry-After").getOrElse(""))
+
+      case status if response.body.isEmpty =>
+        HttpReads.pure(fallback(status, "Error: empty response"))
+
+      case status =>
+        response.header(HeaderNames.CONTENT_TYPE) match {
+          case Some(MimeTypes.JSON) =>
+            if (status >= 200 && status < 300)
+              parseJson[S](response, status)
+            else if (status >= 400)
+              parseJson[F](response, status)
+            else
+              throw UpstreamErrorResponse(s"Unexpected response status $status", 500)
+          case Some(other) =>
+            throw UpstreamErrorResponse(
+              s"Unexpected response type of status $status, expected application/json but got $other with body:\n${response.body}",
+              500
+            )
+          case None =>
+            HttpReads.pure(fallback(status, "Error: missing content-type header"))
+        }
     }
+  }
 
 }
